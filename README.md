361762d1e07d6c6d7952c1e817a72389067b4389
Spectre-NSX <Spectre-NSX@users.noreply.github.com>
2021-08-18 15:57:56 +0100

    Began creating the code to perform functions:
    Functions:
    Check words exist in dictionary,
    Load dictionary file,
    Exit any time the user types quit or exit

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # Blue-Prism-Technical-Test
  This repository is my solution developed to solve the Blue Prism technical test using C#
+
+This readme serves as a logbook for the work done in order to complete the technical test. 
+
+## 18/8/2021
+Upon first reading the project brief, the algorithm to generate the the list of words seemed a little daunting.
+To begin with, I broke the project down into things the application requires in order to function. 
+The aspects I determined were: the project needs to read user input and attempt to open a text file with the name provided by the user.
+The application also needs to accept two words from the user, before creating the shortest list possible of words contained in the dictionary (changing one letter at a time) to move from the starting word, to the end word.
+I decided to start with what I knew how to do (although admittedly I am a little rusty with some aspects of .Net development so did need to check things such as reading user input). I began by layout out the main structure of the application, with placeholder TODO: notes. I filled in everything I could think that the application needs in order to perform its function.
+I then went and began filling out sections of code that I was more familiar with, starting with creating a method that checks if the user has typed the words "quit" or "exit", and exiting the program. I decided whenever the program performs this check, it should convert any string typed by the user to upper case, and then perform the check. This was just to ensure if the user types "Exit" or "QUit" for example, the application would still exit.
--- Technical-Test-Application/Technical-Test-Application/Program.cs
+++ Technical-Test-Application/Technical-Test-Application/Program.cs
@@ -8,81 +8,100 @@ namespace Technical_Test_Application
 {
     class Program
     {
-
-
         static void Main(string[] args)
         {
-
-
             Console.WriteLine("Hello! This application will attempt to find the shortest ---");
             Console.WriteLine("Exit the application at any time by typing quit or exit");
 
-            Console.WriteLine("Loading Dictionary..");
-            
+            // Attempt to load the dictionary
             DictionaryLoad();
 
-            WordsInput();
-
-        }
-
-        static void DictionaryLoad()
-        {
-            // TODO: LOAD DICTIONARY
-            // ARGUMENT OR INSERT INTO APPLICATION??
-
-            // TODO: IF DICTIONARY IS SUCCESSFULLY LOADED:
-            if (1 == 1)
+            // Loop to allow the program to continually function until the user wants to exit.
+            while(true)
             {
-                Console.WriteLine("Dictionary loaded!");
-
-                // DICTIONARY VARIABLE
-                // PASS DICTIONARY TO WORD CHECKS
-            }
-            else
-            {
-                // Dictionary not loaded. Exit
-                Console.WriteLine("There was a problem loading the dictionary. Press any key to exit...");
-                Console.ReadKey();
-                Environment.Exit(1);
-            }
-        }
-
-        static void WordsInput()
-        {
-            // Program main loop
-            // Asks for user input. If the user types quit or exit, application exits
-            while (true)
-            {
-                Console.WriteLine("Please type the first word to check from the dictionary");
-
-
-                // Read user input
-                string userInput = Console.ReadLine();
-                
-                // Save user input as upper case. Used to check if the user wants to exit the application...
-                // ...by typing "exit" or "quit".
-                string exitCheck = userInput.ToUpper();
-                ExitCheck(exitCheck);
-
-                string firstWord = userInput;
-
-                Console.WriteLine("Please type the second word to check from the dictionary");
-                userInput = Console.ReadLine();
-
-                exitCheck = userInput.ToUpper();
-                ExitCheck(exitCheck);
-
-                string secondWord = userInput;
-                CheckDictionary(firstWord, secondWord);
+                // Get the two words from the user.
+                (string firstWord, string secondWord) = WordsInput();
 
                 Console.WriteLine("Your first word is: {0}", firstWord);
                 Console.WriteLine("Your second word is: {0}", secondWord);
 
+                // Check the words exist in the dictionary
+                // TODO: CHECK EACH WORD EXISTS IN THE DICTIONARY
+                CheckDictionary(firstWord);
+                CheckDictionary(secondWord);
+
+                // TODO: GENERATE LIST
+
+                // TODO: WRITE FILE TO OUTPUT
             }
+        }
+
+        // Loads the dictionary into memory
+        static void DictionaryLoad()
+        {
+            bool dictionaryLoaded = false;
+            bool dictionaryPathValid = false;
+
+            // Ask user for directory
+            while (dictionaryLoaded == false)
+            {
+
+
+                Console.WriteLine("Please type the full directory path of the dictionary");
+                string dictionaryPath = Console.ReadLine();
+                // TODO: DICTIONARY VARIABLE
+
+                if(dictionaryPathValid == true)
+                {
+
+                    dictionaryLoaded = true;
+                    Console.WriteLine("Dictionary loaded!");
+                }
+                else
+                {
+                    Console.WriteLine("There was a problem loading the dictionary...");
+                }
+
+
+
+            }
+        }
+
+        // Retrieves the two words to check from the user
+        static (string, string) WordsInput()
+        {
+            // Program main loop
+            // Asks for user input. If the user types quit or exit, application exits
+            Console.WriteLine("Please type the first word to check from the dictionary");
+
+
+            // Read user input
+            string userInput = Console.ReadLine();
+
+            // Save user input as upper case. Used to check if the user wants to exit the application...
+            // ...by typing "exit" or "quit".
+            string exitCheck = userInput.ToUpper();
+            ExitCheck(exitCheck);
+
+            string firstWord = userInput;
+
+            Console.WriteLine("Please type the second word to check from the dictionary");
+            userInput = Console.ReadLine();
+
+            exitCheck = userInput.ToUpper();
+            ExitCheck(exitCheck);
+
+            string secondWord = userInput;
+
+
+
+            return (firstWord, secondWord);
+
 
         }
 
-        static void CheckDictionary(string firstWord, string secondWord)
+        // Checks that the word exists in the dictionary
+        static void CheckDictionary(string word)
         {
             //TODO: CHECK DICTIONARY FOR WORDS
         }
