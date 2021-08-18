d7168f1a7bd3b85e8266e86af33ba77d6666e2ab
Spectre-NSX <Spectre-NSX@users.noreply.github.com>
2021-08-18 12:43:28 +0100

    Application Created

---
+++ Technical-Test-Application/Technical-Test-Application/Program.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Technical_Test_Application
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Press any key to exit...");
+            Console.ReadKey();
+
+        }
+    }
+}
