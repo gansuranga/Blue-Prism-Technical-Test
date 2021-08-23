a9ca38958cdb1485189e2bfb39fb53122a24ac8a
Spectre-NSX <Spectre-NSX@users.noreply.github.com>
2021-08-23 15:52:14 +0100

    Developed: dictionary loading into memory, check each word exists in dictionary, check each word is four letters long.

--- README.md
+++ README.md
@@ -18,3 +18,9 @@ I began by layout out the main structure of the application, with placeholder TO
 I then went and began filling out sections of code that I was more familiar with, starting with creating a method that checks if the user has typed the words "quit" or "exit", and exiting the program. I decided whenever the program performs this check, it should convert any string typed by the user to upper case, and then perform the check. This was just to ensure if the user types "Exit" or "QUit" for example, the application would still exit.
 ....
 I've now worked on getting the user to type the file name and the application will check if the dictionary exists in the Dictionaries folder in the main directory of the application. This gives an error and if the file doesn't exist, asks the user again. 
+
+## 19/8/2021
+Having thought about the process the function should perform to create the lists, I will most likely split this into seperate functions. I have written out the pseudocode for this function, but at the moment it isn't complete.
+
+## 23/8/2021
+Continued working on loading the dictionary and checking each word exists in the dictionary. Also added a check to ensure each word entered is four letters long, and if either word isn't the program will ask again.
--- Technical-Test-Application/Technical-Test-Application/Program.cs
+++ Technical-Test-Application/Technical-Test-Application/Program.cs
@@ -11,112 +11,172 @@ namespace Technical_Test_Application
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello! This application will attempt to find the shortest ---");
+            string[] dictionary = new string[0];
+
+            Console.WriteLine("Hello! This application will attempt to find the shortest path between two words.");
             Console.WriteLine("Exit the application at any time by typing quit or exit");
 
             // Attempt to load the dictionary
-            DictionaryLoad();
+            dictionary = DictionaryLoad();
 
             // Loop to allow the program to continually function until the user wants to exit.
-            while(true)
+            while (true)
             {
+                bool wordOneInDictionary = false;
+                bool wordTwoInDictionary = false;
+
                 // Get the two words from the user.
                 (string firstWord, string secondWord) = WordsInput();
 
                 Console.WriteLine("Your first word is: {0}", firstWord);
                 Console.WriteLine("Your second word is: {0}", secondWord);
 
-                // TODO: CHECK EACH WORD EXISTS IN THE DICTIONARY
                 // Check the words exist in the dictionary
-                CheckDictionary(firstWord);
-                CheckDictionary(secondWord);
+                wordOneInDictionary = DictionaryWordCheck(firstWord, dictionary);
+                wordTwoInDictionary = DictionaryWordCheck(secondWord, dictionary);
 
-                // TODO: GENERATE LIST
-                GenerateList();
+                if (wordOneInDictionary == true && wordTwoInDictionary == true)
+                {
+                    // TODO: GENERATE LIST
+                    GenerateList();
 
-                // TODO: WRITE FILE TO OUTPUT
-                OutputFile();
+                    // TODO: WRITE FILE TO OUTPUT
+                    OutputFile();
+                }
+                else
+                {
+                    // Display words typed that were not found in dictionary.
+                    Console.WriteLine("The following words were not found in the dictionary:");
+                    if (wordOneInDictionary == false)
+                    {
+                        Console.WriteLine(firstWord);
+                    }
+                    if(wordTwoInDictionary == false)
+                    {
+                        Console.WriteLine(secondWord);
+                    }
+                }
             }
         }
 
         // Loads the dictionary into memory
-        static void DictionaryLoad()
+        static string[] DictionaryLoad()
         {
             bool dictionaryLoaded = false;
+            string[] dictionary = new string[0];
 
             // Ask user for the file name of the dictionary they want to use.
             while (dictionaryLoaded == false)
             {
-                Console.WriteLine("Please type the name of the file you wish to use or type quit or exit to quit the application.");
-                Console.WriteLine("Note: The name of the included dictionary is called words-english.txt");
+                // File should be located in a Dictionaries folder in the main application folder.
+                string fileName = "words-english.txt";
                 string currentPath = System.IO.Directory.GetParent(Environment.CurrentDirectory).ToString();
                 string dictionariesPath = Path.GetFullPath(Path.Combine(currentPath, @"..\..\Dictionaries"));
                 //Console.WriteLine(dictionariesPath);
-                string fileName = Console.ReadLine();
+
                 // Save user input as upper case. Used to check if the user wants to exit the application...
                 // ...by typing "exit" or "quit".
                 string exitCheck = fileName.ToUpper();
                 ExitCheck(exitCheck);
 
+                // Full path of the file
                 string filePath = Path.Combine(dictionariesPath, fileName);
-                //Console.WriteLine(filePath);
-                
+
+                // If file exists in path...
                 if (File.Exists(filePath))
                 {
-                    // TODO: TRY/CATCH LOAD DICTIONARY
-                    // TODO: IF DICTIONARY IS LOADED: CONTINUE. ELSE GIVE ERROR MESSAGE
-                    dictionaryLoaded = true;
-                    Console.WriteLine("Dictionary loaded!");
+                    // Try to load the file, or exit the application
+                    try
+                    {
+                        dictionary = System.IO.File.ReadAllLines(filePath);
+                        dictionaryLoaded = true;
+                        Console.WriteLine("Dictionary loaded!");
+                    }
+                    catch
+                    {
+                        Console.WriteLine("There was an error loading the file. Press any key to exit");
+                        Console.ReadKey();
+                        Environment.Exit(0);
+                    }
                 }
+                // If the file does not exist in the correct path, exit the application.
                 else
                 {
                     Console.WriteLine("\nThe following file does not exist: {0}", fileName);
-                    Console.WriteLine("Please ensure the file is in the following directory:\n{0}\n", dictionariesPath);
+                    Console.WriteLine("The dictionary file should be located in:\n{0}\n", dictionariesPath);
+
+                    Console.WriteLine("Press any key to exit.");
+                    Console.ReadKey();
+                    Environment.Exit(0);
                 }
             }
+            return dictionary;
         }
 
         // Asks user to input two words and returns them.
         static (string, string) WordsInput()
         {
-            // Asks for user for two words. These are used to search the dictionary
-            Console.WriteLine("Please type the first word to check from the dictionary");
+            string userInput;
+            string exitCheck;
+            string firstWord = string.Empty;
+            string secondWord = string.Empty;
+            bool wordOneValid = false;
+            bool wordTwoValid = false;
 
-            // Read user input (first word)
-            string userInput = Console.ReadLine();
+            // Continually ask for first word until user inputs a four letter word.
+            while (wordOneValid == false)
+            {
+                // Asks for user for two words. These are used to search the dictionary
+                Console.WriteLine("Please type the first word to check from the dictionary.");
+                
+                // Read user input (first word)
+                userInput = Console.ReadLine();
+                // Save user input as upper case. Used to check if the user wants to exit the application...
+                // ...by typing "exit" or "quit".
+                exitCheck = userInput.ToUpper();
+                ExitCheck(exitCheck);
+                // Check the word is four letters long.
+                wordOneValid = ValidWordsCheck(userInput);
+                firstWord = userInput;
+            }
 
-            // Save user input as upper case. Used to check if the user wants to exit the application...
-            // ...by typing "exit" or "quit".
-            string exitCheck = userInput.ToUpper();
-            ExitCheck(exitCheck);
-
-            string firstWord = userInput;
- 
-            // Asks for user input (second word)
-            Console.WriteLine("Please type the second word to check from the dictionary");
-            userInput = Console.ReadLine();
-
-            exitCheck = userInput.ToUpper();
-            ExitCheck(exitCheck);
-
-            string secondWord = userInput;
-
-            // TODO: CHECK THAT THE ENTERED WORDS ARE 4 CHARACTERS LONG - NEW METHOD
-
+            // Continually ask for second word until user inputs a four letter word.
+            while (wordTwoValid == false)
+            {
+                // Asks for user input (second word)
+                Console.WriteLine("Please type the second word to check from the dictionary.");
+                userInput = Console.ReadLine();
+                // Perform exit check.
+                exitCheck = userInput.ToUpper();
+                ExitCheck(exitCheck);
+                // Check second word is four letters long.
+                wordTwoValid = ValidWordsCheck(userInput);
+                secondWord = userInput;
+            }
             return (firstWord, secondWord);
         }
 
         // Checks that the word exists in the dictionary
-        static void CheckDictionary(string word)
+        static bool DictionaryWordCheck(string word, string[] dictionary)
         {
-            // TODO: CHECK DICTIONARY FOR WORDS
+            bool wordPresent = false;
+            foreach(string line in dictionary)
+            {
+                if (word.ToLower() == line.ToLower())
+                {
+                    wordPresent = true;
+                }
+            }
+            return (wordPresent);
         }
 
+        // Generates a 2D array of paths
         static void GenerateList()
         {
             // TODO: Generate list using the two words given by the user
         }
 
+        // Outputs the result to a file.
         static void OutputFile()
         {
             // TODO: SET FILE DIRECTORY
@@ -131,6 +191,32 @@ namespace Technical_Test_Application
             {
                 Environment.Exit(0);
             }
+        }
+
+        // Checks that an entered word is four characters long.
+        static bool ValidWordsCheck(string word)
+        {
+            bool wordValid = false;
+
+            if (word.Length == 4)
+            {
+                wordValid = true;
+            }
+            else
+            {
+                Console.WriteLine("This word is not four letters long.");
+            }
+
+            return (wordValid);
+        }
+
+        static string AskUserForFileName()
+        {
+            Console.WriteLine("Please type the name of the file you wish to use or type quit or exit to quit the application.");
+            Console.WriteLine("Note: The name of the included dictionary is called words-english.txt");
+            string fileName = Console.ReadLine();
+
+            return (fileName);
         }
     }
 }
